f36c7fa6ab7f017b7b9bbe3f311130c1527f1b4f
496372916 <496372916@qq.com>
2018-07-24 23:22:03 +0800

    第五次commit

---
+++ c/ch_1/entab_1_21.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#define MAXIMUM 1000
+// getchar()那里有问题
+
+int get_input(char c[],int length);
+void entab(char c[], int space_nums, char rc[]);
+
+int main()
+{
+    int length, space_nums;
+    char c[MAXIMUM], rc[MAXIMUM];
+    
+    length = get_input(c, length);
+    
+    space_nums = 4;
+    // entab
+    entab(c, space_nums, rc);
+    printf("\n%s",c);
+	printf("\n%s\n",rc);
+}
+
+int get_input(char c[], int length)
+{
+    int i;
+	char ch;
+    
+    
+    for(i=0;(ch=getchar())!=EOF && i < length; i++)
+    {   
+		c[i] = ch;
+		printf("%c",ch);
+	}
+	printf("\n%s",c);
+    
+    if(i < length)
+        c[i] = '\0';
+    return i;
+}
+
+// entab
+void entab(char c[], int space_nums, char rc[])
+{
+    int i, j, k, count;
+    
+    count = 0;
+    j = 0;
+    
+    for(i=0; c[i]!='\0';i++ )
+    {
+        if(c[i]!=' ')
+        {
+            for(k=0;k<count;k++)
+            {
+                rc[j] = ' ';
+                j++;
+            }
+            count = 0;
+            rc[j] = c[i];
+            j++;
+            continue;
+        }
+        count++;
+        if(count == space_nums)
+        {
+            rc[j] = '\t';
+            j++;
+            count = 0;
+        }
+    }
+}
